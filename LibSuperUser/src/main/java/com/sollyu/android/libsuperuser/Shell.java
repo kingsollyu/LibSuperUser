aa1cd0fd54665648a180230cef417832c462313f
sollyu <sollyu@qq.com>
2017-07-26 10:34:00 +0800

    update

--- LibSuperUser/src/main/java/com/sollyu/android/libsuperuser/Shell.java
+++ LibSuperUser/src/main/java/com/sollyu/android/libsuperuser/Shell.java
@@ -2,7 +2,6 @@ package com.sollyu.android.libsuperuser;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 
@@ -13,12 +12,19 @@ import java.util.LinkedList;
  */
 
 public class Shell {
-    private static final String TAG = "Shell";
 
+    @SuppressWarnings("WeakerAccess")
     public static class Result {
         private Integer            exitCode    = Integer.MIN_VALUE;
         private LinkedList<String> output      = new LinkedList<>();
         private LinkedList<String> errorOutput = new LinkedList<>();
+
+        public Result() {
+        }
+
+        public Result(Integer exitCode) {
+            this.exitCode = exitCode;
+        }
 
         public Integer getExitCode() {
             return exitCode;
@@ -35,28 +41,27 @@ public class Shell {
 
     public static class Sh {
 
+        public synchronized static Result run(String command){
+            Result result = new Result(-1);
+            try {
+                Process shProcess = Runtime.getRuntime().exec(command);
 
-        public synchronized static Result run(String command) throws IOException, InterruptedException {
-            Result result = new Result();
-            Process shProcess = Runtime.getRuntime().exec(command);
+                BufferedReader successResult = new BufferedReader(new InputStreamReader(shProcess.getInputStream()));
+                BufferedReader errorResult   = new BufferedReader(new InputStreamReader(shProcess.getErrorStream()));
 
+                String outputLine;
+                while ((outputLine = successResult.readLine()) != null) {
+                    result.getOutput().add(outputLine);
+                }
 
-            BufferedReader successResult = new BufferedReader(new InputStreamReader(shProcess.getInputStream()));
-            BufferedReader errorResult   = new BufferedReader(new InputStreamReader(shProcess.getErrorStream()));
+                while ((outputLine = errorResult.readLine()) != null) {
+                    result.getErrorOutput().add(outputLine);
+                }
 
-            String outputLine;
-            while ((outputLine = successResult.readLine()) != null) {
-                //Log.d(TAG, outputLine);
-                result.getOutput().add(outputLine);
+                shProcess.waitFor();
+                result.exitCode = shProcess.exitValue();
+            } catch (Throwable ignored) {
             }
-
-            while ((outputLine = errorResult.readLine()) != null) {
-                //Log.e(TAG, outputLine);
-                result.getErrorOutput().add(outputLine);
-            }
-
-            shProcess.waitFor();
-            result.exitCode = shProcess.exitValue();
             return result;
         }
     }
@@ -66,35 +71,38 @@ public class Shell {
         private static Process          suProcess = null;
         private static DataOutputStream STDIN     = null;
 
-        public synchronized static Result run(String command) throws IOException {
-            Result result = new Result();
+        public synchronized static Result run(String command) {
+            Result result = new Result(-1);
 
-            if (suProcess == null) {
-                suProcess = Runtime.getRuntime().exec("su");
-                STDIN = new DataOutputStream(suProcess.getOutputStream());
-            }
-
-            STDIN.write((command + "\n").getBytes("UTF-8"));
-            STDIN.write(("echo 1DCCADFED7BCBB036C56A4AFB97E906F $?\n").getBytes("UTF-8"));
-
-            BufferedReader successResult = new BufferedReader(new InputStreamReader(suProcess.getInputStream()));
-            BufferedReader errorResult   = new BufferedReader(new InputStreamReader(suProcess.getErrorStream()));
-
-            String outputLine;
-            while ((outputLine = successResult.readLine()) != null) {
-                if (outputLine.startsWith("1DCCADFED7BCBB036C56A4AFB97E906F ")) {
-                    result.exitCode = Integer.valueOf(outputLine.substring("1DCCADFED7BCBB036C56A4AFB97E906F ".length()));
-                    break;
+            try {
+                if (suProcess == null) {
+                    suProcess = Runtime.getRuntime().exec("su");
+                    STDIN = new DataOutputStream(suProcess.getOutputStream());
                 }
-                result.getOutput().add(outputLine);
-            }
 
-            STDIN.write(("echo 1DCCADFED7BCBB036C56A4AFB97E906F $? >&2\n").getBytes("UTF-8"));
-            while ((outputLine = errorResult.readLine()) != null) {
-                if (outputLine.startsWith("1DCCADFED7BCBB036C56A4AFB97E906F ")) {
-                    break;
+                STDIN.write((command + "\n").getBytes("UTF-8"));
+                STDIN.write(("echo 1DCCADFED7BCBB036C56A4AFB97E906F $?\n").getBytes("UTF-8"));
+
+                BufferedReader successResult = new BufferedReader(new InputStreamReader(suProcess.getInputStream()));
+                BufferedReader errorResult   = new BufferedReader(new InputStreamReader(suProcess.getErrorStream()));
+
+                String outputLine;
+                while ((outputLine = successResult.readLine()) != null) {
+                    if (outputLine.startsWith("1DCCADFED7BCBB036C56A4AFB97E906F ")) {
+                        result.exitCode = Integer.valueOf(outputLine.substring("1DCCADFED7BCBB036C56A4AFB97E906F ".length()));
+                        break;
+                    }
+                    result.getOutput().add(outputLine);
                 }
-                result.getErrorOutput().add(outputLine);
+
+                STDIN.write(("echo 1DCCADFED7BCBB036C56A4AFB97E906F $? >&2\n").getBytes("UTF-8"));
+                while ((outputLine = errorResult.readLine()) != null) {
+                    if (outputLine.startsWith("1DCCADFED7BCBB036C56A4AFB97E906F ")) {
+                        break;
+                    }
+                    result.getErrorOutput().add(outputLine);
+                }
+            } catch (Throwable ignored) {
             }
 
             return result;
